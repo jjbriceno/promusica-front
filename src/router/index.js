ef037303412ac67b15293f775f54d2fa8fd41934
José Briceño <josejorgebriceno9@gmail.com>
2022-10-29 22:20:06 -0400

    Initial commit

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+
 Vue.use(VueRouter)
 
 const routes = [
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import(/* webpackChunkName: "login" */ '../views/Login.vue')
   },
   {
     path: '/about',
---
+++ src/views/Login.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-container fill-height>
+    <v-row align="center" justify="center">
+      <v-col cols="12" md="8">
+        <div> {{ user }}</div>
+        <v-form>
+          <v-card>
+            <v-toolbar color="primary" dark> Login </v-toolbar>
+            <v-card-text>
+              <v-text-field
+                name="email"
+                label="Email"
+                id="email"
+                type="text"
+                v-model="form.email"></v-text-field>
+              <v-text-field
+                name="password"
+                label="Password"
+                id="password"
+                type="password"
+                v-model="form.password"></v-text-field>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn color="success" @click="login">Login</v-btn>
+              <v-spacer></v-spacer>
+              <v-btn color="primary">Cancel</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-form>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<script>
+export default {
+  data: () => ({
+    user: {},
+    form: {
+      email: '',
+      password: '',
+    },
+  }),
+  methods: {
+    login() {
+      const vm = this;
+      console.log('Login in...', vm.form);
+    },
+  },
+};
+</script>
